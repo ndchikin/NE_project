60b472b67fa9a9ab54a4ec3ef342d1fbe2d1126f
LinkToHell <nikolajchikin@yandex.ru>
2020-06-27 17:03:42 +0300

    update

--- start.sh
+++ start.sh
@@ -1,2 +1,1 @@
-(./filter_players | ./players_to_PPH | ./to_PPH | ./solve_PPH.py) < graphs.txt > orders.txt
-(./graph_gen | ./players_gen_filtered | ./to_PH | ./process_cnf.py) < graphs.txt
+(./graph_gen | ./players_gen | ./to_PH | ./process_cnf.py) < graphs.txt
