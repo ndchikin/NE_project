9e89011b263d5b9894087f5e6fbb9c7b32934b03
ndchikin <nikolajchikin@yandex.ru>
2020-08-27 14:18:29 +0300

    Readme update

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+sudo apt-get update
+sudo apt-get install graphviz
+sudo apt-get install python3-dev
+sudo apt-get install python3-pip
+python3 -m pip install cmake
--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/bin/sh
 (./filter_players | ./players_to_PPH | ./to_PPH | ./solve_PPH.py) < graphs.txt > orders.txt
 (./graph_gen | ./players_gen_filtered | ./to_PH | ./process_cnf.py) < graphs.txt
